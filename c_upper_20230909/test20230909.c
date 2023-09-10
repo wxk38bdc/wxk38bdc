4517a7f7dd8d7fe80c43a2fe18c00d536e2f5035
wxk38bdc <2227814847@qq.com>
2023-09-10 21:32:22 +0800

    daily submission

--- c_upper_20230909/test20230909.c
+++ c_upper_20230909/test20230909.c
@@ -3,9 +3,51 @@
 #include<string.h>
 #include<math.h>
 
+//int main()
+//{
+//	unsigned int i = 020462340677;
+//		printf("%d\n", i);
+//	return 0;
+//}
+
+//检测大端字节序/小端字节序
+//int main()
+//{
+//	int a = 1;
+//	int b = *(char*)&a;
+//	printf("%d\n", b);//b=1，表明int类型为小端字节序存储
+//	return 0;
+//}
+
+////exercise_1
+//int main()
+//{
+//	char a = -1;//-1
+//	signed char b = -1;//-1
+//	unsigned char c = -1;//255
+//	printf("a=%d,b=%d,c=%d ", a, b, c);//%d对应有符号整型，使用char类型要进行整形提升
+//	return 0;
+//}
+
+//exercise_2
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+//exercise_3
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);
+//	return 0;
+//}
+
 int main()
 {
 
 	return 0;
 }
-
