a867d87b2ce592a8d8a8cc9ec623015a30e29960
wxk38bdc <2227814847@qq.com>
2023-09-15 22:43:39 +0800

    submission 20230915_2

--- c_upper_20230915/test.c
+++ c_upper_20230915/test.c
@@ -4,8 +4,25 @@
 #include<math.h>
 #include<windows.h>
 
-int main()
-{
-
-	return 0;
-}
+//int main()
+//{
+//	//int a[4] = { 1,2,3,4 };
+//	//printf("%d", sizeof(&a+1));
+//	//char arr[] = { 'a','b','c','d','e','f' };
+//	char arr[] = "abcdef";
+///*	printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr + 0));
+//	printf("%d\n", sizeof(*arr));
+//	printf("%d\n", sizeof(arr[1]));
+//	printf("%d\n", sizeof(&arr));
+//	printf("%d\n", sizeof(&arr + 1));
+//	printf("%d\n", sizeof(&arr[0] + 1));*/	
+//	printf("%d\n", strlen(arr));
+//	printf("%d\n", strlen(arr + 0));
+////	printf("%d\n", strlen(*arr));//±®¥Ì£¨“∞÷∏’Î
+////	printf("%d\n", strlen(arr[1]));
+//	printf("%d\n", strlen(&arr));
+//	printf("%d\n", strlen(&arr + 1));
+//	printf("%d\n", strlen(&arr[0] + 1));
+//	return 0;
+//}
