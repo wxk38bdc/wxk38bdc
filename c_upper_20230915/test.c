fc508564c010f2e5db311ea57d239a4a67d99ee1
wxk38bdc <2227814847@qq.com>
2023-09-17 10:36:24 +0800

    daily submission 20230917

--- c_upper_20230915/test.c
+++ c_upper_20230915/test.c
@@ -27,13 +27,125 @@
 //	return 0;
 //}
 
-int main()
-{
-	int i = 0;
-	for (i = 1; i <=10; i++)
-	{
-		printf("你的生命浪费了%d秒\n",i);
-		Sleep(1000);
-	}
-	return 0;
-}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <=10; i++)
+//	{
+//		printf("你的生命浪费了%d秒\n",i);
+//		Sleep(1000);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3][4] = { 0 };
+//	printf("%d\n", sizeof(a));//48
+//	printf("%d\n", sizeof(a[0][0]));//4
+//	printf("%d\n", sizeof(a[0]));//16
+//	printf("%d\n", sizeof(a[0] + 1));//4
+//	printf("%d\n", sizeof(*(a[0] + 1)));//4
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5] = { 1, 2, 3, 4, 5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d,%d", *(a + 1), *(ptr - 1));
+//	return 0;
+//}
+
+//struct Test
+//{
+//	int Num;
+//	char* pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}*p=(struct Test*)0x100000;
+////假设p 的值为0x100000。 如下表表达式的值分别为多少？
+////已知，结构体Test类型的变量大小是20个字节//x86环境
+//int main()
+//{
+//	printf("%p\n", p );
+//	printf("%p\n", p + 0x1);
+//	printf("%p\n", (unsigned long)p + 0x1);
+//	printf("%p\n", (unsigned int*)p + 0x1);
+//	return 0;
+//}
+
+////笔试题3
+//int main()
+//{
+//	int a[4] = { 1, 2, 3, 4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x,%x", ptr1[-1], *ptr2);//4；2000000
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3][2] = { (0, 1), (2, 3), (4, 5) };//逗号表达式的结果是最后一个表达式的结果
+//	int* p=a[0];
+//	printf("%d", a[0][0]);//1
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);//p[4][2]相当于*(*(p+4)+2)
+//	return 0;
+//}
+
+//int main()
+//{
+//	/*int time = 0;
+//	printf("请输入时间（单位：秒）\n");
+//	scanf("%d", &time);*/
+//	printf("你的电脑将在120秒后关机\n");
+//	system("shutdown -s -t 120");
+//	int i = 0;
+//	printf("输入520，救你一命！\n");
+//	scanf("%d", &i);
+//	if (i == 520)
+//	{
+//		system("shutdown -a");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);//at
+//	return 0;
+//}
+
+//int main()//令人懵逼的题目
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//	printf("%s\n", **++cpp);//POINT
+//	printf("%s\n", *-- * ++cpp + 3);//ER
+//	printf("%s\n", *cpp[-2] + 3);//ST
+//	printf("%s\n", cpp[-1][-1] + 1);//EW
+//	return 0;
+//}
