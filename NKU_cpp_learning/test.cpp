ae97b159fe272ad56572aec00b9d3b3850d5c1dd
wxk38bdc <2227814847@qq.com>
2023-09-21 15:52:01 +0800

    daily submission 20230921

---
+++ NKU_cpp_learning/test.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>//i-o-stream ÊäÈëÊä³öÁ÷
+#include<stdio.h>
+using namespace std;
+int main()
+{
+		
+	return 0;
+}
---
+++ 新文件1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 0;
+	int y = 0;
+	cin >> x >> y;
+	cout << x *y << endl;
+	return 0;
+}
