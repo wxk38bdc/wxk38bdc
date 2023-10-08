42e7978bd8caf5d2709fe5c02fe417fa05fd03f2
wxk38bdc <2227814847@qq.com>
2023-10-09 07:58:11 +0800

    submission 20231009

--- project_20231008/test.c
+++ project_20231008/test.c
@@ -1,6 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
 //int main()
 //{
 //	printf("Hello World!\n");
@@ -26,11 +25,56 @@
 //	printf("%s\n",arr2);
 //	return 0;
 //}
+////实现两个矩阵的相乘
+//int main()
+//{
+//	//输入矩阵的行数，列数
+//	int n, m, k, i, j;
+//	scanf("%d %d%d", &n, &m, &k);
+//    int a[100][100], b[100][100], c[100][100];
+//	//输入矩阵
+//	for(i=0;i<n;i++)
+//	{
+//		for(j=0;j<m;j++)
+//		{
+//			scanf("%d", &a[i][j]);
+//		}
+//	}
+//	for(i=0;i<m;i++)
+//	{
+//		for(j=0;j<k;j++)
+//		{
+//			scanf("%d", &b[i][j]);
+//		}
+//	}
+//	//计算矩阵相乘
+//	for(i=0;i<n;i++)
+//	{
+//		for(j=0;j<k;j++)
+//		{
+//			c[i][j] = 0;
+//			for(int t=0;t<m;t++)
+//			{
+//				c[i][j] += a[i][t] * b[t][j];
+//			}
+//		}
+//	}
+//	//输出矩阵
+//	for(i=0;i<n;i++)
+//	{
+//		for(j=0;j<k;j++)
+//		{
+//			printf("%d ", c[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
 
+#include <stdio.h>
 int main()
 {
 
 	return 0;
 }
-
 
