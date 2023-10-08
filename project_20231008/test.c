c1fbbbe0afb36c6976e26de0bd251d9b64e0cc70
wxk38bdc <2227814847@qq.com>
2023-10-09 07:39:55 +0800

    daily submission 20231009

---
+++ project_20231008/test.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//int main()
+//{
+//	printf("Hello World!\n");
+//	return 0;
+//}
+
+//打印从32到127的ASCII码
+//int main()
+//{
+//	int i = 0;
+//	for(i=32;i<=127;i++)
+//	{
+//		printf("%c\n",i);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = {'a','b','c'};
+//	printf("%s\n",arr1);
+//	printf("%s\n",arr2);
+//	return 0;
+//}
+
+int main()
+{
+
+	return 0;
+}
+
+
