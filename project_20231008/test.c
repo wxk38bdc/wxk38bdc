b9c6124c2da93b844cb490fc47f8707812460ad4
wxk38bdc <2227814847@qq.com>
2023-10-11 13:21:38 +0800

    daily submission 20231011

--- project_20231008/test.c
+++ project_20231008/test.c
@@ -121,36 +121,156 @@
 //}
 
 //像素点模糊化处理
+//int main()
+//{
+//	int n,m, i, j;
+//	scanf("%d %d", &n, &m);
+//	double a[101][101];
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%lf", &a[i][j]);
+//		}
+//	}
+//	int k = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			if (i == 0 || j == 0 || i == n - 1 || j == m - 1)
+//			{
+//				printf("%.0lf ", a[i][j]);
+//			}
+//			else
+//			{
+//				printf("%.0lf ",(a[i][j]+a[i-1][j]+a[i+1][j]+a[i][j-1]+a[i][j+1])/5*1.0);
+//			}
+//			
+//		}
+//		printf("\n");
+//	}
+//	//printf("%.0lf", 5.6);
+//	return 0;
+//}
+
+//NKUoj test1
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	for(int i=2*n;i>0;i-=2)
+//	{
+//		for(int j=0;j<(2*n-i)/2;j++)
+//		{
+//			printf(" ");
+//		}
+//		for(int k=0;k<i;k++)
+//		{
+//			printf("*");
+//		}
+//
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include<stdbool.h>
+//int main()
+//{
+//	bool flag = true;
+//	if (flag)
+//		printf("true\n");
+//	else
+//		printf("false\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d %d ", a+b, a-b, a*b);
+//	if (b == 0)
+//		printf("error");
+//	else
+//	{ 
+//		printf("%d", a / b);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b, c;
+//	scanf("%d %d %d", &a, &b, &c);
+//	int max=a, min=a;
+//	if(b > max)
+//		max = b;
+//	if(c > max)
+//		max = c;
+//	if(b<min)
+//		min = b;
+//	if(c<min)
+//		min = c;
+//	printf("%d %d %d", max, min,a+b+c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3];
+//	int i;
+//	for(i=0;i<3;i++)
+//	{
+//		scanf("%d", &a[i]);
+//	}
+//	for(i=0;i<3;i++)
+//	{
+//		if (a[i] >= 90 && a[i] <= 100)
+//			printf("A ");
+//		else if(a[i] >= 80 && a[i] < 90)
+//			printf("B ");
+//		else if(a[i] >= 70 && a[i] < 80)
+//			printf("C ");
+//		else if(a[i] >= 60 && a[i] < 70)
+//			printf("D ");
+//		else
+//			printf("E ");
+//	}
+//	printf("%d ", (a[0] + a[1] + a[2])/3);
+//	return 0;
+//}
+
+//十进制数转换为二进制数
+//#include <stdio.h>
+//int main()
+//{
+//	int a, i=0;
+//	int arr[100];
+//	scanf("%d",&a);
+//	while(a)
+//	{
+//		arr[i]= a % 2;
+//		a /= 2;
+//		i++;
+//	}
+//	for(int j=i-1;j>=0;j--)
+//	{
+//		printf("%d", arr[j]);
+//	}
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//    printf("%d\n", strlen("c:\test\121"));
+//    return 0;
+//}
+
 #include <stdio.h>
 int main()
 {
-	int n,m, i, j;
-	scanf("%d %d", &n, &m);
-	double a[101][101];
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			scanf("%lf", &a[i][j]);
-		}
-	}
-	int k = 0;
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < m; j++)
-		{
-			if (i == 0 || j == 0 || i == n - 1 || j == m - 1)
-			{
-				printf("%.0lf ", a[i][j]);
-			}
-			else
-			{
-				printf("%.0lf ",(a[i][j]+a[i-1][j]+a[i+1][j]+a[i][j-1]+a[i][j+1])/5*1.0);
-			}
-			
-		}
-		printf("\n");
-	}
-	//printf("%.0lf", 5.6);
 	return 0;
 }
